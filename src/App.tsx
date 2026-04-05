3630a83e1ceeaf914ea27e9cc8fd6c3c3acd5e94
IvanTsve <iv_plamenovv@abv.bg>
2026-04-05 18:14:53 +0300

    Application added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Application } from '@pixi/react';
+import { useRef } from 'react';
+
 function App() {
-  return <></>
+  const parentRef = useRef(null);
+  return(
+  <div ref={parentRef} style={{ width: '100%', height: '100%' }}>
+    <Application resizeTo={parentRef} />
+  </div>
+  )
 }
 
 export default App
