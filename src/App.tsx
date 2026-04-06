8fa97590aabc8c9140c7caae58661ade44876482
IvanTsve <iv_plamenovv@abv.bg>
2026-04-06 20:03:48 +0300

    add click btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Application, extend } from '@pixi/react';
-import { Container, Assets } from 'pixi.js';
+import { Container, Assets, Text } from 'pixi.js';
 import SlotReel from './components/SlotReel.jsx';
 import { useState, useEffect } from 'react';
 
@@ -12,28 +12,39 @@ import six from './assets/6.png';
 
 extend({
   Container,
+  Text,
 });
 
-export default function App() { 
+export default function App() {
   const [textures, setTextures] = useState([]);
   useEffect(() => {
     Assets.load([one, two, three, four, five, six]).then((textures) => {
-      setTextures([ Object.values(textures)[0], Object.values(textures)[1], Object.values(textures)[2], Object.values(textures)[3], Object.values(textures)[4], Object.values(textures)[5] ]);
+      setTextures([Object.values(textures)[0], Object.values(textures)[1], Object.values(textures)[2], Object.values(textures)[3], Object.values(textures)[4], Object.values(textures)[5]]);
     });
-   
+
   }, []);
   return (
     <Application width={800} height={600} defaultTextStyle={{ fontSize: 24, fontWeight: 'bold', color: '#000' }}>
-      <pixiContainer 
-       position={{ x: 300, y: 100 }}
-       scale={1.5}
+      <pixiContainer
+        position={{ x: 300, y: 100 }}
+        scale={1.5}
       >
-      {
-        Array.from({ length: 1 }).map((_, index) => (
-          <SlotReel key={index} textures={textures} posX={150*index} />
-        ))
-      }
+        {
+          Array.from({ length: 1 }).map((_, index) => (
+            <SlotReel key={index} textures={textures} posX={150 * index} />
+          ))
+        }
       </pixiContainer>
+      <pixiText
+        text="Spin"
+        position={{ x: 300, y: 500 }}
+        eventMode="static"
+        cursor="pointer"
+        style={{ fontSize: 24, fontWeight: 'bold', fill: '#fff' }}
+        onClick={() => {
+          console.log('clicked');
+        }}
+      />
     </Application>
   );
 }
