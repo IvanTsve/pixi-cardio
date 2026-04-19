acaab2c3004f9e9bc4660e8aa4a6d34fdc5e2ec9
IvanTsve <iv_plamenovv@abv.bg>
2026-04-19 18:58:16 +0300

    Update IndexDemo.jsx

--- src/components/demo/IndexDemo.jsx
+++ src/components/demo/IndexDemo.jsx
@@ -1,8 +1,38 @@
+import { Application } from '@pixi/react';
+import { Assets } from 'pixi.js';
+import { useEffect, useState } from 'react';
+import Reels from './Reels';
 
 export default function IndexDemo() {
+    const [textures, setTextures] = useState([]);
+    const slotImages = Object.values(
+        import.meta.glob('../../assets/slots/*.png', {
+            eager: true,
+            import: 'default',
+        }),
+    );
+
+    useEffect(() => {
+    Assets.load(slotImages).then((textures) => {
+        setTextures(textures);
+    });
+    }, []);
+
     return (
-        <div>
-            <h1>Demo</h1>
-        </div>
+    <Application width={800} height={600} defaultTextStyle={{ fontSize: 24, fontWeight: 'bold', color: '#000' }}>
+       
+        <Reels slotImages={textures} />
+        <pixiText
+            text="Play"
+            position={{ x: 300, y: 500 }}
+            eventMode="static"
+            cursor="pointer"
+            style={{ fontSize: 24, fontWeight: 'bold', fill: '#fff' }}
+            onClick={() => {
+                console.log('Play');
+            }}
+        />
+
+    </Application>
     )
 }
